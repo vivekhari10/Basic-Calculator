ba864046965920ce547bd6dbd07f3d5e1c77436b
vivekhari10 <vivekh499@gmail.com>
2023-05-25 22:45:24 +0530

    first commit

---
+++ calcu.py
@@ -0,0 +1,14 @@
+print("Enter the first number")
+num1=int(input())
+print("Enter the Second number")
+num2=int(input())
+print("Select the operator from 1.Add 2.Sub 3.Mul 4.Div")
+op=input()
+if(op=="1"):
+ print("Sum is", num1+num2)
+if(op=="2"):
+ print("Diff is",num1-num2)
+if(op=="3"):
+ print("Product is",num1*num2)
+if(op=="4"):
+ print("Result is",num1/num2)
